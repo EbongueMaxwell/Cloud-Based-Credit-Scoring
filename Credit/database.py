d63d348ff8209040d7ffeef090c2fc62b4971031
EbongueMaxwell <nsamemaxwell00@gmail.com>
2025-05-27 23:46:32 +0100

    Delete Credit/database.py

--- Credit/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://ebongue:ebongue00@localhost/credit_app"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
